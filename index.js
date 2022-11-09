38462c1790f9376f8639c426ae52de3bb658cf41
BrianZ26 <brian.zhao026@gmail.com>
2022-11-09 13:58:58 -0500

    a

--- index.js
+++
@@ -1,6 +0,0 @@
-const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  input: document.querySelector("#input"),
-};
-DOMSelectors.box.insertAdjacentHTML("");
