bb5188bb6edfce509ba56875f9d330898f7e2aca
BrianZ26 <brian.zhao026@gmail.com>
2022-11-15 13:59:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   title: document.getElementById("title"),
   artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
   form: document.getElementById("form"),
   display: document.getElementById("display"),
 };
-DOMSelectors.form.addEventListener(
-  "submit",
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  input();
+});
+function input() {
   DOMSelectors.display.insertAdjacentHTML(
     "beforeend",
-    '<div class="display-card"><img class="display-img" src="${url}" /><h2 class="display-artist">"${artist}"</h2> <h3 class="display-album">"${title}"</h3> <button class="remove btn">Remove Album</button> </div>'
-  )
-);
+    `<div class="display-card">
+  <img class="display-img" src=${DOMSelectors.url.value} /> 
+  <h2 class="display-artist">${DOMSelectors.artist.value}</h2> 
+  <h3 class="display-album">${DOMSelectors.title.value}</h3> 
+  <button class="remove btn" id="btn">Remove Album</button> 
+  </div>`
+  );
+}
